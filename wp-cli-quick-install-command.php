197275ad594b1d0fda8989e56297b8111bfc7c82
Matthias Kadenbach <matthias.kadenbach@gmail.com>
2014-03-04 22:28:43 +0100

    some tests

--- wp-cli-quick-install-command.php
+++ wp-cli-quick-install-command.php
@@ -1,15 +1,21 @@
 <?php
-
-class Quick_Install_Command extends WP_CLI_Command {
+/**
+ * Quickly install WordPress
+ */
+class Quick_Command extends WP_CLI_Command {
 
   /**
-   *  @when before_wp_load
+   * Get information about WP-CLI itself.
+   *
+   * @when before_wp_load
    */
-  function test($args, $assoc_args) {
-    WP_CLI::warning("Hello!");
+  function install($args, $assoc_args) {
+    $skipped_plugins = \WP_CLI::get_runner()->extra_config["core install"];
+
+    WP_CLI::warning(print_r($skipped_plugins, true));
   }
 
 }
 
-WP_CLI::add_command( 'quick-install', 'Quick_Install_Command' );
+WP_CLI::add_command( 'quick', 'Quick_Command' );
 
